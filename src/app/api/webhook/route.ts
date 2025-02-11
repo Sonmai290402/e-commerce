658d1d8147a5cc387318dae7de338e519c46361a
MAI THẾ SƠN <maitheson@192.168.1.12.non-exists.ptr.local>
2025-02-11 22:11:54 +0700

    modify route

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,14 +1,18 @@
-import { WebhookEvent } from "@clerk/nextjs/dist/types/server";
+import createUser from "@/lib/actions/user.action";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-
 export async function POST(req: Request) {
-  const svix_id = req.headers.get("svix-id") ?? "";
-  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
-  const svix_signature = req.headers.get("svix-signature") ?? "";
+  const svix_id = headers().get("svix-id") ?? "";
+  const svix_timestamp = headers().get("svix-timestamp") ?? "";
+  const svix_signature = headers().get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not set");
   }
-
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Bad Request", { status: 400 });
+  }
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
@@ -23,15 +27,25 @@ export async function POST(req: Request) {
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    return new Response("Bad request", err);
+    return new Response("Bad Request", { status: 400 });
   }
 
   const eventType = msg.type;
   if (eventType === "user.created") {
-    //create user to database
-    console.log(msg.data);
+    // create user to database
+    const { id, username, email_addresses, image_url } = msg.data;
+    const user = await createUser({
+      username: username!,
+      name: username!,
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      avatar: image_url,
+    });
+    return NextResponse.json({
+      message: "OK",
+      user,
+    });
   }
-  // Rest
 
   return new Response("OK", { status: 200 });
 }
