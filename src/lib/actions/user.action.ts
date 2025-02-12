557f5bdbc38c87b600095c6f39ffcee8982f6967
MAI THẾ SƠN <maitheson@192.168.1.12.non-exists.ptr.local>
2025-02-12 14:27:15 +0700

    edit connect mongodb

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -2,14 +2,14 @@
 
 import User from "@/database/user.modal";
 import { TCreateUserParams } from "@/types";
-import { connectToDatabase } from "../mongoose";
+import { connect } from "../mongoose";
 
 export default async function createUser(params: TCreateUserParams) {
   try {
-    connectToDatabase();
+    await connect();
     const newUser = await User.create(params);
     return newUser;
   } catch (error) {
-    console.log("Error while creating user: ", error);
+    console.error("Error creating user:", error);
   }
 }
