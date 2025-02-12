4d69514fca653034b8051bea94ad8af5e4e4b4b2
MAI THẾ SƠN <maitheson@192.168.1.12.non-exists.ptr.local>
2025-02-12 14:30:31 +0700

    edit mongoose.ts

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -7,27 +7,29 @@ interface MongooseConn {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConn = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+// Khai báo kiểu dữ liệu cho biến global
+declare global {
+  var mongoose: MongooseConn | undefined;
 }
+
+// Khởi tạo cached từ biến toàn cục
+let cached: MongooseConn = global.mongoose || { conn: null, promise: null };
 
 export const connect = async () => {
   if (cached.conn) return cached.conn;
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
       dbName: "e-commerce",
       bufferCommands: false,
       connectTimeoutMS: 30000,
     });
+  }
 
   cached.conn = await cached.promise;
+
+  // Lưu cached vào biến global để tái sử dụng
+  global.mongoose = cached;
 
   return cached.conn;
 };
