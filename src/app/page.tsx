a91bc591172473ca2d999e132fd6a5ed464cc812
MAI THẾ SƠN <maitheson@192.168.1.51.non-exists.ptr.local>
2025-02-13 16:43:47 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Banner from "@/components/layout/Banner";
 import Header from "@/components/layout/Header";
-import createUser from "@/lib/actions/user.action";
 
 export default async function Home() {
   return (
